84dae2e7c9a459ee8836198e0f0b294adf033e9a
Prashant Kawade <prashant.kawade@nuance.com>
2020-07-08 11:39:46 +0530

    Problem 2 and 3

---
+++ prashant/src/Arrays/BestTimeToBuyAndSellStock.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Arrays
+{
+	class BestTimeToBuyAndSellStock
+	{
+        public int MaxProfitNSquare(int[] prices)
+        {
+            int profit = 0;
+
+            for (int i = 0; i < prices.Length; i++)
+            {
+                for (int j = i + 1; j < prices.Length; j++)
+                {
+                    int diff = prices[j] - prices[i];
+                    if (diff > 0 && diff > profit)
+                    {
+                        profit = diff;
+                    }
+                }
+            }
+
+            return profit;
+        }
+
+        public int MaxProfitN(int[] prices)
+        {
+            int min = int.MaxValue;
+            int maxProfit = 0;
+
+            for (int i = 0; i < prices.Length; i++)
+            {
+                if (prices[i] < min)
+                {
+                    min = prices[i];
+                }
+                else if (prices[i] - min > maxProfit)
+                {
+                    maxProfit = prices[i] - min;
+                }
+            }
+
+            return maxProfit;
+        }
+    }
+}
---
+++ prashant/src/Arrays/ContainsDuplicate.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections.Generic;
+
+namespace Arrays
+{
+	class ContainsDuplicate
+	{
+        public bool ContainsDuplicateN(int[] nums)
+        {
+            var dict = new Dictionary<int, int>();
+
+            foreach (var i in nums)
+            {
+                if (dict.ContainsKey(i))
+                {
+                    return true;
+                }
+                else
+                {
+                    dict[i] = i;
+                }
+            }
+
+            return false;
+        }
+    }
+}
